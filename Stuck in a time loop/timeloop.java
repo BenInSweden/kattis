c484ea03743be186a6e0f5f605f98f8b20425303
Ben Evans <evans.ben@gmail.com>
2018-01-15 11:22:09 +0100

    test

---
+++ Stuck in a time loop/timeloop.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Timeloop {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int input = in.nextInt();
+
+        System.out.println(input);
+    }
+}
