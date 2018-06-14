d46e637fa4416ca5fabfe9542aa0e5b58477d091
BenInSweden <evans.ben@gmail.com>
2018-06-14 21:50:04 +0200

    Create TrollHunt.java

---
+++ Troll Hunt/TrollHunt.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    String input = sc.nextLine();
+	    String[] params = input.split(" ");
+	    int output;
+
+        int bridges = Integer.parseInt(params[0])-1;
+        int knights = Integer.parseInt(params[1]);
+        int knightsPerGroup = Integer.parseInt(params[2]);
+
+        if(bridges%(knights/knightsPerGroup)>0) {
+            output = (bridges/(knights/knightsPerGroup))+1;
+        }else output = bridges/(knights/knightsPerGroup);
+
+        System.out.println(output);
+    }
+}
